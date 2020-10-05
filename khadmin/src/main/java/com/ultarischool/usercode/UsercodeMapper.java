dfac5dca2f9cf1d4078a609f7410054a48859368
taehyun <taehyun@woojooae.com>
2020-10-06 07:22:29 +0900

    init

--- khadmin/src/main/java/com/ultarischool/usercode/UsercodeMapper.java
+++
@@ -1,9 +0,0 @@
-package com.ultarischool.usercode;
-
-import java.util.List;
-
-import org.apache.ibatis.annotations.Param;
-
-public interface UsercodeMapper {
-  List<UsercodeVO> selectByCode3(@Param("code3") String code3);
-}
